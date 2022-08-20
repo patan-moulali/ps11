b5efbb2cb516ca4f80912c5112e396d69c3c5440
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-20 18:43:24 +0530

    Add project files.

---
+++ ps11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nInput first integer");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nInput second integer");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("check if both are positive");
+            Console.WriteLine((x > 0 && y > 0) || (x > 0 && y > 0));
+
+            Console.ReadLine();
+        }
+    }
+}
